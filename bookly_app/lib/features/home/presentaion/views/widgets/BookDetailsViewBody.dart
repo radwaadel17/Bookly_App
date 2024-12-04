a9e4039d10aa9f15c3b062f5a50f06aca3d277d1
radwaadel17 <radwaadel959@gmail.com>
2024-12-04 22:02:43 +0200

    create custom app bar for book details view

--- bookly_app/lib/features/home/presentaion/views/widgets/BookDetailsViewBody.dart
+++ bookly_app/lib/features/home/presentaion/views/widgets/BookDetailsViewBody.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/presentaion/views/widgets/CustomAppBarForBookDetailsView%20.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -6,7 +7,10 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [],
+      children: [
+        CustomAppBarForBookDetailsView(),
+      ],
     );
   }
 }
+
