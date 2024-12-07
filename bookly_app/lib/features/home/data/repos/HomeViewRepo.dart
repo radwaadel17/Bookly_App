c7f062ac51f2f5b01c7793b78c555da8f53fe794
radwaadel17 <radwaadel959@gmail.com>
2024-12-07 22:00:46 +0200

    create faluire model and handle methods in home view repo

--- bookly_app/lib/features/home/data/repos/HomeViewRepo.dart
+++ bookly_app/lib/features/home/data/repos/HomeViewRepo.dart
@@ -1,6 +1,9 @@
+// ignore: file_names
+import 'package:bookly_app/core/errors/faluiremodel.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class Homeviewrepo {
-  Future<List<BookModel>> fetchNewstBooks();
-  Future<List<BookModel>> fetchBestSellerBooks();
+  Future<Either<Faluire, List<BookModel>>> fetchNewstBooks();
+  Future<Either<Faluire, List<BookModel>>> fetchBestSellerBooks();
 }
