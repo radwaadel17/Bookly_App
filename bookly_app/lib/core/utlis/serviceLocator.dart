cf87de71e1b4014394e0f6416abbc3acef6b16b4
radwaadel17 <radwaadel959@gmail.com>
2024-12-08 23:16:29 +0200

    create service locator and redactor provider of cubit ans apply singleton

---
+++ bookly_app/lib/core/utlis/serviceLocator.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/core/utlis/apiService.dart';
+import 'package:bookly_app/features/home/data/repos/HomeViewRepImp.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setup() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+
+  getIt.registerSingleton<Homeviewrepimp>(Homeviewrepimp(
+    getIt<ApiService>(),
+  ));
+}
--- bookly_app/lib/main.dart
+++ bookly_app/lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utlis/apiService.dart';
 import 'package:bookly_app/core/utlis/approuter.dart';
+import 'package:bookly_app/core/utlis/serviceLocator.dart';
 import 'package:bookly_app/features/home/data/repos/HomeViewRepImp.dart';
 import 'package:bookly_app/features/home/presentaion/manager/BestSellerBooksCubit/BestSellerCubit.dart';
 import 'package:bookly_app/features/home/presentaion/manager/NewestBooksCubit/NewestBooksCubit.dart';
-import 'package:bookly_app/features/splash/presentaion/views/spalsh%20view.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
+  setup();
   runApp(const BooklyApp());
 }
 
@@ -22,21 +20,15 @@ class BooklyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) =>
-              NewestbooksCubit(
-                Homeviewrepimp(
-                  ApiService(
-                    Dio()))),
-          
+          create: (context) => NewestbooksCubit(
+            getIt<Homeviewrepimp>(),
           ),
-              BlocProvider(
-          create: (context) =>
-              BestSellerCubit(
-                Homeviewrepimp(
-                  ApiService(
-                    Dio()))),
-          
+        ),
+        BlocProvider(
+          create: (context) => BestSellerCubit(
+            getIt<Homeviewrepimp>(),
           ),
+        ),
       ],
       child: MaterialApp.router(
         routerConfig: Approuter.router,
