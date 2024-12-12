0f62645b594aa91aacd9ff8dbbc52c325ce27a75
radwaadel17 <radwaadel959@gmail.com>
2024-12-12 22:49:27 +0200

    create search repo implementation

--- bookly_app/lib/features/search/data/repos/searchRepo.dart
+++ bookly_app/lib/features/search/data/repos/searchRepo.dart
@@ -4,5 +4,5 @@ import 'package:bookly_app/features/home/data/models/book_model/book_model.dart'
 import 'package:dartz/dartz.dart';
 
 abstract class Searchrepo {
-  Future<Either<Faluire , List<BookModel>>> fetchSearchResults(); 
+  Future<Either<Faluire , List<BookModel>>> fetchSearchResults({required String searchWord}); 
 }
---
+++ bookly_app/lib/features/search/data/repos/searchRepoIm.dart
@@ -0,0 +1,30 @@
+// ignore: file_names
+import 'package:bookly_app/core/errors/faluiremodel.dart';
+import 'package:bookly_app/core/utlis/apiService.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/search/data/repos/searchRepo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class Searchrepoim implements Searchrepo {
+  @override
+  Future<Either<Faluire, List<BookModel>>> fetchSearchResults(
+      {required String searchWord}) async {
+    try {
+      var data = await ApiService(Dio()).getData(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=relevance &q=subject:$searchWord');
+      List<dynamic> dataList = data['items'];
+      List<BookModel> books = [];
+      for (int i = 0; i < dataList.length; i++) {
+        books.add(BookModel.fromJson(dataList[i]));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.FromDioError(e));
+      }
+      return left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
+}
