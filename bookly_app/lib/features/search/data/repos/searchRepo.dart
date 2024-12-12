3c51b239709cd21395b3448fba735feffc4e2886
radwaadel17 <radwaadel959@gmail.com>
2024-12-12 22:40:44 +0200

    create search repo

---
+++ bookly_app/lib/features/search/data/repos/searchRepo.dart
@@ -0,0 +1,8 @@
+// ignore: file_names
+import 'package:bookly_app/core/errors/faluiremodel.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class Searchrepo {
+  Future<Either<Faluire , List<BookModel>>> fetchSearchResults(); 
+}
